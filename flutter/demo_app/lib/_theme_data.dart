6db48d7ce4ee6695356d4396cfc412c6682bc6b5
y <yeatsinheart@gmail.com>
2025-07-29 06:16:41 +0800

    1

--- flutter/demo_app/lib/_theme_data.dart
+++ flutter/demo_app/lib/_theme_data.dart
@@ -8,7 +8,26 @@ byRem(v,context){
 /// 如果有缓存，那么需要使用缓存数据。怎么判断有没有缓存，或者某key为空时。。
 getThemeData(context) {
   // 默认rem计算
+  // 名称	典型用途	默认大小（Material 3）
+  // displayLarge	页标题（超大）	57
+  // displayMedium	页面主标题	45
+  // displaySmall	页面副标题	36
 
+  // headlineLarge	大号标题	32
+  // headlineMedium	标题	28
+  // headlineSmall	副标题	24
+
+  // titleLarge	内容区主标题	22
+  // titleMedium	内容区子标题	16
+  // titleSmall	小标题	14
+
+  // bodyLarge	正文大	16
+  // bodyMedium	正文	14
+  // bodySmall	辅助信息	12
+
+  // labelLarge	按钮文本	14
+  // labelMedium	辅助按钮文本	12
+  // labelSmall	小号按钮文字	11
   TextStyle globalTextStyle = TextStyle(fontSize: byRem(.22,context),height: 1.5,color: Colors.deepPurpleAccent);
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,

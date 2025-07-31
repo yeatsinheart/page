338b6ea1edeedd5ebac41b4bc42af393ffb52e55
y <yeatsinheart@gmail.com>
2025-08-01 06:36:10 +0800

    1

--- flutter/demo_app/lib/style/setting/setting-container-demo.dart
+++ flutter/demo_app/lib/style/setting/setting-container-demo.dart
@@ -11,14 +11,18 @@ var setting = {
     padding: Padding(top: 0),
     bgGradient: gradient_linear(colors: ["#7ec1f7", "#FFF"], stops: [0, .8], begin: top_right, end: bottom_left),
   ),
+
   "color1": container(bg: "#2196F3", font: "#fff"),
   "color1-reverse": container(
     bg: "#2196F3",
     border: Border(color: "#2196F3"),
   ),
+
   "img-loading": container(bg: "#e0e0e0", font: "#fff"),
   "marquee": container(padding: Padding(left: 0.2, right: 0.2), bg: "transparent", font: "#000"),
+
   "game-category-tab": container(bg: "#fff", font: "#000"),
+
   "txt-cover": container(
     bgGradient: gradient_linear(colors: ["#0000004d", "transparent"], stops: [0, .8], begin: bottom_center, end: top_center),
     font: "#fff",

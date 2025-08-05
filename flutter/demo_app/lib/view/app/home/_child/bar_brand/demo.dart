c5705817d7fc01b6803e9f28e162c27093b99b20
y <yeatsinheart@gmail.com>
2025-08-06 05:13:02 +0800

    1

--- flutter/demo_app/lib/view/app/home/_child/bar_brand/demo.dart
+++ flutter/demo_app/lib/view/app/home/_child/bar_brand/demo.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter3/app-route.dart';
 import 'package:flutter3/i18n.dart';
 import 'package:flutter3/share/img.dart';
-import 'package:flutter3/share/overlay.dart';
 import 'package:flutter3/store/app.dart';
 import 'package:flutter3/style/format/container.dart';
 
@@ -19,13 +18,50 @@ class BarBrandDemo extends StatelessWidget {
         children: [
           AppImg('assets/images/jinritoutiao.png', width: AppStore.byRem(2.1), height: AppStore.byRem(0.7)),
           Spacer(),
-          ContainerFormat("btn-register", Text('注册'.t),click: ()=> {AppRoute.to("auth",params: {"action":"register"})},),
+          ContainerFormat(
+            "btn-register",
+            Text('注册'.t),
+            click: () => {
+              AppRoute.to("auth", params: {"action": "register"}),
+            },
+          ),
           const SizedBox(width: 8),
-          ContainerFormat("btn-login", Text('登录'.t),click: ()=> {AppRoute.to("auth",params: {"action":"login"})},),
-          SizedBox(width: AppStore.byRem(.2)),
-          IconButton(icon:Icon(Icons.search),onPressed: ()=>AppRoute.slideToKey("game_search")),
-          SizedBox(width: AppStore.byRem(.2)),
-          IconButton(icon:Icon(Icons.language),onPressed: ()=>AppRoute.to("language")),
+          ContainerFormat(
+            "btn-login",
+            Text('登录'.t),
+            click: () => {
+              AppRoute.to("auth", params: {"action": "login"}),
+            },
+          ),
+          SizedBox(width: AppStore.byRem(.1)),
+
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => AppRoute.slideToKey("game_search"),
+            style: ButtonStyle().copyWith(
+              padding: WidgetStateProperty.all(EdgeInsets.all(0)),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  // borderRadius: BorderRadius.circular(AppStore.byRem(.14)),
+                  side: BorderSide(color: Colors.transparent),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: AppStore.byRem(.1)),
+          IconButton(
+            icon: Icon(Icons.language),
+            onPressed: () => AppRoute.to("language"),
+            style: ButtonStyle().copyWith(
+              padding: WidgetStateProperty.all(EdgeInsets.all(0)),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  // borderRadius: BorderRadius.circular(AppStore.byRem(.14)),
+                  side: BorderSide(color: Colors.transparent),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );

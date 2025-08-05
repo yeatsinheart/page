a67064c309e04e782a4fc7dea79a7710f371ff25
y <yeatsinheart@gmail.com>
2025-08-06 05:07:06 +0800

    1

--- flutter/demo_app/lib/view/game/search/demo.dart
+++ flutter/demo_app/lib/view/game/search/demo.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter3/share/grid.dart';
-import 'package:flutter3/share/img.dart';
 import 'package:flutter3/share/share_widget.dart';
 import 'package:flutter3/share/sliver_header_delegate.dart';
 import 'package:flutter3/store/app.dart';
 import 'package:flutter3/style/format/container.dart';
-
 
 class GameSearchDemo extends StatefulWidget {
   const GameSearchDemo({super.key, required params});
@@ -21,15 +19,21 @@ class _GameSearchDemoState extends State<GameSearchDemo> with TickerProviderStat
 
   build_search() {
     return Padding(
-      padding: const EdgeInsets.all(12),
-      child: TextField(
-        decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
-          hintText: "搜索",
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-          filled: true,
-          // fillColor: Colors.grey[200],
-        ),
+      padding: EdgeInsets.all(AppStore.byRem(.2)),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "搜索",
+                // fillColor: Colors.grey[200],
+              ),
+            ),
+          ),
+          SizedBox(width: AppStore.byRem(.1)),
+          TextButton(onPressed: () {}, child: Text("搜索"), style: ButtonStyle().copyWith()),
+        ],
       ),
     );
   }
@@ -42,38 +46,48 @@ class _GameSearchDemoState extends State<GameSearchDemo> with TickerProviderStat
         body: SafeArea(
           child: CustomScrollView(
             slivers: [
-              // 搜索框
-              SliverToBoxAdapter(child: build_search()),
-
-              // tab header
               SliverPersistentHeader(
                 pinned: true,
                 delegate: ShareSliverPersistentHeaderDelegate(
-                  height: 48,
-                  child: tab_bar_demo(tabs: tabs, currentIndex: _currentIndex, onTap: (index) => setState(() => _currentIndex = index)),
+                  height: AppStore.byRem(1.65),
+                  child: ContainerFormat(
+                    "tab",
+                    Column(
+                      children: [
+                        build_search(),
+                        tab_bar_demo(tabs: tabs, currentIndex: _currentIndex, onTap: (index) => setState(() => _currentIndex = index)),
+                      ],
+                    ),
+                  ),
                 ),
               ),
 
               // 当前 Tab 对应的内容
-              SliverPadding( padding: EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)),
-                  sliver: grid_sliver_demo(List.generate(22, (_)=>tabs[_currentIndex]),numberOfRow: 3)),
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)),
+                sliver: grid_sliver_demo(List.generate(22, (_) => tabs[_currentIndex]), numberOfRow: 3),
+              ),
 
               // 最近热门标题
               SliverToBoxAdapter(
                 child: Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)),
-                  child: Text("最近热门", style: TextStyle(fontSize: AppStore.byRem(.26), fontWeight: FontWeight.bold)),
+                  padding: EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)),
+                  child: Text(
+                    "最近热门",
+                    style: TextStyle(fontSize: AppStore.byRem(.26), fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
 
               // 最近热门 Grid
-              SliverPadding( padding: EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)), sliver: grid_sliver_demo(List.generate(22, (_)=>"最近热门"),numberOfRow: 3)),
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: AppStore.byRem(.2), vertical: AppStore.byRem(.1)),
+                sliver: grid_sliver_demo(List.generate(22, (_) => "最近热门"), numberOfRow: 3),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
-
 }

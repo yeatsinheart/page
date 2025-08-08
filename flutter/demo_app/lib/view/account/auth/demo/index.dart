5b782e6cd7c0b28fc32b4323c3cc5bd3711ea903
y <yeatsinheart@gmail.com>
2025-08-08 14:35:15 +0800

    1

--- flutter/demo_app/lib/view/account/auth/demo/index.dart
+++ flutter/demo_app/lib/view/account/auth/demo/index.dart
@@ -25,9 +25,6 @@ class _State extends State<AccountAuthDemoIndex> with SingleTickerProviderStateM
   final tabs = ['login', 'register'];
   var active;
   var type = "name"; // "phone"
-  var _key = GlobalKey();
-  double contentHeight=0;
-  final ScrollController _scrollController = ScrollController();
   @override
   void initState() {
     super.initState();
@@ -35,7 +32,6 @@ class _State extends State<AccountAuthDemoIndex> with SingleTickerProviderStateM
     final initialIndex = tabs.indexOf(active);
     _tabController = TabController(length: tabs.length, vsync: this, initialIndex: initialIndex >= 0 ? initialIndex : 0);
     _tabController.addListener(() {
-      realHeight();
       // 会触发两次，一次动画中，一次动画完成
       if (_tabController.indexIsChanging) {
         // 点击触发，但动画尚未完成 这时候切换，动画效果更流畅
@@ -48,104 +44,77 @@ class _State extends State<AccountAuthDemoIndex> with SingleTickerProviderStateM
       }
     });
   }
-  realHeight(){
-    //WidgetsBinding.instance.addPostFrameCallback((_) {
-      final context = _key.currentContext;
-      if (context != null) {
-        final box = context.findRenderObject() as RenderBox;
-        final size = box.size;
-        contentHeight=size.height.clamp(0.0, AppStyle.screenHeight * .8);;
-      }
-    //});
-  }
+
   @override
   Widget build(BuildContext context) {
-    realHeight();
-    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-    /// 理论上要 content + keyboard = bottom padding + visible
-    final total = contentHeight + keyboardHeight ;
-    /// 总体不能超过屏幕高
-    final over = total - AppStyle.screenHeight ;
-    //Log.i("$total ${AppStyle.screenHeight} $over");
-    double padding = keyboardHeight;
-    if(over<=0)padding=(keyboardHeight + over).clamp(0.0, keyboardHeight);
-    /// 允许的 padding（不能小于 0）
-    if(over>0)padding =  (keyboardHeight - over).clamp(0.0, keyboardHeight);
-    //_scrollController.jumpTo(padding.clamp(0.0, _scrollController.position.maxScrollExtent));
-    //Log.i("$contentHeight $keyboardHeight ${AppStyle.screenHeight*.8} $padding");
-    return AnimatedPadding(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-      padding: EdgeInsets.only(bottom: padding),
-      child: SingleChildScrollView(
-        controller: _scrollController,
-        child: ContainerFormat(
-          key:_key,
-          "section",
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: AppStyle.byRem(2),
-                child: Img("assets/images/game1.jpg", borderRadiusTopLeft: AppStyle.radius, borderRadiusTopRight: AppStyle.radius),
-              ),
-              TabBar(
-                controller: _tabController,
-                tabs: tabs.map((e) => Tab(height: AppStyle.byRem(.5), text: e.t)).toList(),
-              ),
+    return ContainerFormat(
+      padding: EdgeInsetsGeometry.all(0),
+      margin: EdgeInsetsGeometry.all(0),
+      "container",
+      SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: AppStyle.byRem(2),
+              child: Img("assets/images/game1.jpg", borderRadiusTopLeft: AppStyle.radius, borderRadiusTopRight: AppStyle.radius),
+            ),
+            TabBar(
+              controller: _tabController,
+              tabs: tabs.map((e) => Tab(height: AppStyle.byRem(.5), text: e.t)).toList(),
+            ),
 
-              Container(
-                padding: EdgeInsetsGeometry.all(AppStyle.gap),
-                child: Column(
-                  children: [
-                    type == "name" && active == "login" ? LoginByName() : SizedBox.shrink(),
-                    type == "name" && active == "register" ? RegisterByName() : SizedBox.shrink(),
-                    type == "phone" && active == "login" ? LoginByPhone() : SizedBox.shrink(),
-                    type == "phone" && active == "register" ? RegisterByPhone() : SizedBox.shrink(),
-                  ],
-                ),
+            Container(
+              padding: EdgeInsetsGeometry.all(AppStyle.gap),
+              child: Column(
+                children: [
+                  type == "name" && active == "login" ? LoginByName() : SizedBox.shrink(),
+                  type == "name" && active == "register" ? RegisterByName() : SizedBox.shrink(),
+                  type == "phone" && active == "login" ? LoginByPhone() : SizedBox.shrink(),
+                  type == "phone" && active == "register" ? RegisterByPhone() : SizedBox.shrink(),
+                ],
               ),
+            ),
 
-              //Divider(),
-              Container(
-                height: AppStyle.gap + AppStyle.fontSize,
-                child: TextButton(
-                  onPressed: () {
-                    type = type == 'phone' ? 'name' : 'phone';
-                    setState(() {});
-                  },
-                  child: Center(child: Text("切换 ${type == 'phone' ? '用户名' : '手机号'} ${active == 'login' ? '登陆' : '注册'}")),
-                  // style: ButtonStyle().copyWith(overlayColor: WidgetStateProperty.all(Colors.transparent)),
-                ),
+            //Divider(),
+            Container(
+              height: AppStyle.gap + AppStyle.fontSize,
+              child: TextButton(
+                onPressed: () {
+                  type = type == 'phone' ? 'name' : 'phone';
+                  setState(() {});
+                },
+                child: Center(child: Text("切换 ${type == 'phone' ? '用户名' : '手机号'} ${active == 'login' ? '登陆' : '注册'}")),
+                // style: ButtonStyle().copyWith(overlayColor: WidgetStateProperty.all(Colors.transparent)),
               ),
+            ),
 
-              Divider(),
+            Divider(),
 
-              Container(
-                height: AppStyle.gap + AppStyle.fontSize * AppStyle.lineHeight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Center(child: Text("谷歌")), // center能拉伸高度
-                      ),
+            Container(
+              height: AppStyle.gap + AppStyle.fontSize * AppStyle.lineHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Center(child: Text("谷歌")), // center能拉伸高度
                     ),
-                    SizedBox(width: AppStyle.byRem(.2)),
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Center(child: Text("facebook")), // center能拉伸高度
-                      ),
+                  ),
+                  SizedBox(width: AppStyle.byRem(.2)),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Center(child: Text("facebook")), // center能拉伸高度
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          padding: EdgeInsetsGeometry.all(0),
+            ),
+          ],
         ),
+
       ),
     );
   }
--- flutter/demo_app/lib/view/pop/close_bottom.dart
+++ flutter/demo_app/lib/view/pop/close_bottom.dart
@@ -7,8 +7,34 @@ import 'package:flutter3/style/format/container.dart';
 
 class PopCloseBottom extends StatelessWidget {
   final dynamic params;
+  final _key = GlobalKey();
+  double contentHeight = 0;
 
-  const PopCloseBottom({this.params, super.key});
+  PopCloseBottom({this.params, super.key});
+
+  realHeight() {
+    final context = _key.currentContext;
+    if (context != null) {
+      final box = context.findRenderObject() as RenderBox;
+      contentHeight = box.size.height.clamp(0.0, AppStyle.screenHeight * .8);
+    }
+  }
+
+  _get_keyboard_padding(context) {
+    realHeight();
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
+    /// 理论上要 content + keyboard = bottom padding + visible
+    /// 总体不能超过屏幕高
+    final gap = AppStyle.screenHeight * .1;
+    final total = contentHeight + keyboardHeight;
+    final over = total - AppStyle.screenHeight;
+    //Log.i("$contentHeight $keyboardHeight  ${AppStyle.screenHeight * .8} gap $gap over $over");
+    double padding = keyboardHeight;
+    if (over <= 0) padding = (keyboardHeight + over).clamp(0.0, keyboardHeight);
+    if (over > 0) padding = (keyboardHeight - over + gap).clamp(0.0, keyboardHeight);
+    return padding;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +44,27 @@ class PopCloseBottom extends StatelessWidget {
         Positioned.fill(
           child: IgnorePointer(child: Container(color: Colors.black.withValues(alpha: 0.4))),
         ),
-        Column(mainAxisAlignment: MainAxisAlignment.center, children: [_content(), _close()]),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [_content(_get_keyboard_padding(context)), _close()]),
       ],
     );
   }
 
-  _content() {
+  _keyboard_wrap(child, padding) {
+    return AnimatedPadding(
+      duration: const Duration(milliseconds: 150),
+      curve: Curves.easeOut,
+      padding: EdgeInsets.only(bottom: padding),
+      child: child,
+    );
+  }
+
+  _content(padding) {
     return ConstrainedBox(
+      key: _key,
       constraints: BoxConstraints(minWidth: AppStyle.byPercent(10), maxWidth: AppStyle.byPercent(80), minHeight: AppStyle.screenHeight * .1, maxHeight: AppStyle.screenHeight * .8),
       child: DefaultTextStyle.merge(
         style: TextStyle(color: ColorFont.get().txt),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(AppStyle.radius),
-          child: params ?? Container(color: Colors.red),
-          //SafeArea(child: SingleChildScrollView(child: ),
-        ),
+        child: _keyboard_wrap(ClipRRect(borderRadius: BorderRadius.circular(AppStyle.radius), child: params), padding),
       ),
     );
   }

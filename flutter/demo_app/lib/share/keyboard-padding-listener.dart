bed1bf3e96e48ebdccc59ca8270e529d1d5e79ac
y <yeatsinheart@gmail.com>
2025-08-08 17:07:07 +0800

    1

--- flutter/demo_app/lib/share/keyboard-padding-listener.dart
+++ flutter/demo_app/lib/share/keyboard-padding-listener.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter3/app-style.dart';
 import 'package:flutter3/log/logger.dart';
@@ -5,6 +7,7 @@ import 'package:flutter3/log/logger.dart';
 class KeyboardPaddingListener extends StatelessWidget {
   final Widget child;
   final double maxHeight;
+  double childHeight = 0;
 
   KeyboardPaddingListener(this.child, {double? maxHeight}) : this.maxHeight = maxHeight ?? AppStyle.screenHeight;
 
@@ -25,25 +28,25 @@ class KeyboardPaddingListener extends StatelessWidget {
     final context = _key.currentContext;
     if (context != null) {
       final box = context.findRenderObject() as RenderBox;
-      return box.size.height.clamp(0.0, maxHeight);
+      childHeight = max(childHeight, box.size.height.clamp(0.0, maxHeight));
     }
     return 0;
   }
 
   double _get_keyboard_padding(context) {
-    /// 垂直居中 上下距离
-    double gap = (AppStyle.screenHeight - maxHeight) / 2;
-
     double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-    if (0 == keyboardHeight) return 0;
+    if (0 == keyboardHeight) {
+      childHeight = 0;
+      return 0;
+    }
+    realHeight();
+    double gap = (AppStyle.screenHeight - maxHeight) / 2;
+    double overflow = childHeight - keyboardHeight - AppStyle.screenHeight;
 
-    /// 实际有用的padding ：键盘高度 - 上下距离
-    double effect_padding = keyboardHeight - gap;
-    double height_after_padding = realHeight() + effect_padding;
-
-    /// 超出屏幕部分
-    double over_height = height_after_padding - maxHeight - gap;
-    // Log.i("$keyboardHeight effect_padding $effect_padding height_after_padding $height_after_padding over_height $over_height");
-    return (keyboardHeight + over_height).clamp(0.0, keyboardHeight);
+    if (overflow <= 0) {
+      return (keyboardHeight + overflow).clamp(0.0, keyboardHeight);
+    }
+    Log.i(overflow);
+    return (keyboardHeight - overflow + gap).clamp(0.0, keyboardHeight);
   }
 }

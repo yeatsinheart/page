9aa509159f6d159a4191fcd233f58d91db90152c
y <yeatsinheart@gmail.com>
2025-08-09 21:56:31 +0800

    1

--- flutter/demo_app/lib/service/cache.dart
+++ flutter/demo_app/lib/service/cache.dart
@@ -3,17 +3,21 @@ import 'package:hive/hive.dart';
 
 class CacheService {
   static final Map<String, Box> _map = {};
-
+  static final version=0;
+  static String version_control(String table){
+    return "${table}_${version}";
+  }
   static Future<Box> _openTable(String table) async {
-    if (_map.containsKey(table)) {
-      return _map[table]!;
+    String tableName = version_control(table);
+    if (_map.containsKey(tableName)) {
+      return _map[tableName]!;
     }
     try {
-      var box = await Hive.openBox(table);
-      _map[table] = box;
+      var box = await Hive.openBox(tableName);
+      _map[tableName] = box;
       return box;
     } catch (e) {
-      Log.err('Failed to open Hive box $table: ', e);
+      Log.err('Failed to open Hive box $tableName: ', e);
       rethrow; // 或者返回一个默认值
     }
   }
